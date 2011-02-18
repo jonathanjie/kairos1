1f435484faac9f090f64120f7e920bae1975b9f5
Jonathan <jonathanjie@gmail.com>
2011-02-18 21:32:21 +0800

    replace 'int' with 'long int'

--- src/Lab4q3a.c
+++ src/Lab4q3a.c
@@ -2,23 +2,23 @@
 
 int main( void ){
 
-	int num=0;
-	int count=0;
-	int ans=1;
+	long num = 0;
+	long count = 0;
+	long ans = 1;
 
 	printf("Enter a positive integer: \n");
-	scanf("%d", &num);
+	scanf("%lu", &num);
 	
-	count=num;
+	count = num;
 	
 	if(num == 0) {
-		printf("0 != 1\n");
+		printf("0! = 1\n");
 	} else if(num > 0) {
 		while(count > 0) {
-			ans*=count;
+			ans *= count;
 			count--;
 		}
-		printf("%d != %d\n", num, ans); 
+		printf("%lu! = %lu\n", num, ans); 
 	} else if (num < 0 || num % 1 > 0) {
 		printf("The number you entered was invalid. Please try again.\n");
 	}
